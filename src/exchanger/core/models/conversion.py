c36c39fa8c63b7cd489066a183d2d1b4c40c692a
maff1337 <yakdm@duck.com>
2026-06-19 00:34:45 +0300

    feat: add conversion models

---
+++ src/exchanger/core/models/conversion.py
@@ -0,0 +1,25 @@
+from decimal import Decimal
+from dataclasses import dataclass
+
+from exchanger.core.models.currency import Currency
+from exchanger.core.vo.exchange_pair import ExchangePair
+
+
+@dataclass(frozen=True)
+class RequestConversion:
+    exchange_pair: ExchangePair
+    amount: Decimal
+
+    def __post_init__(self) -> None:
+        if self.amount < Decimal(0):
+            raise ValueError(
+                f'Amount cannot be negative. Amount - {self.amount}')
+
+
+@dataclass(frozen=True)
+class ResponseConversion:
+    base: Currency
+    target: Currency
+    rate: Decimal
+    amount: Decimal
+    converted_amount: Decimal
