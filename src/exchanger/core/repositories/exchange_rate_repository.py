d0862f6b99dc8e7bc2df528d62a8dcc7a0f4a3cf
maff1337 <yakdm@duck.com>
2026-07-01 21:49:45 +0300

    fix: remove unused imports, remove conversion method

--- src/exchanger/core/repositories/exchange_rate_repository.py
+++ src/exchanger/core/repositories/exchange_rate_repository.py
@@ -3,7 +3,6 @@ from typing import Protocol, Sequence
 
 from exchanger.core.vo.exchange_pair import ExchangePair
 from exchanger.core.models.exchange_rate import ExchangeRate
-from exchanger.core.models.conversion import RequestConversion, ResponseConversion
 
 
 class ExchangeRateRepository(Protocol):
@@ -17,8 +16,3 @@ class ExchangeRateRepository(Protocol):
 
     @abstractmethod
     def find_all(self) -> Sequence[ExchangeRate]: ...
-
-    @abstractmethod
-    def convert(
-        self, request_conversion: RequestConversion
-    ) -> ResponseConversion: ...
